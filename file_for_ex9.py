45041d4ae57cc3a440936e64a6a880fd01c7b07f
waki-zashi <xxeeggoorr@gmail.com>
2024-09-20 12:17:32 +0300

    Update file_for_ex9.py

--- file_for_ex9.py
+++ file_for_ex9.py
@@ -1,10 +1,11 @@
 k = 0
 m = 0
 for i in range(1000, 10000):
-    i = str(i)
+        i = str(i)
     if int(i[0]) % 3 == 0:
-
-@@ -9,5 +13,9 @@ for i in range(1000, 10000):
+        r = int(i[0]) + int(i[2]) + abs(int(i[1]) - int(i[3]))
+    else:
+        r = (bin(int(''.join(sorted(i))))[2:]).count('1')
 
     if r > 10:
         k += 1
