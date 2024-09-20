b737fe4133afd6dade0bdacadab76aa36b514f49
waki-zashi <xxeeggoorr@gmail.com>
2024-09-20 12:04:10 +0300

    Create file_for_ex9.py

---
+++ file_for_ex9.py
@@ -0,0 +1,11 @@
+k = 0
+for i in range(1000, 10000):
+    i = str(i)
+    if int(i[0]) % 2 == 0:
+        r = int(i[0]) + int(i[2]) + abs(int(i[1]) - int(i[3]))
+    else:
+        r = (bin(int(''.join(sorted(i))))[2:]).count('1')
+
+    if r > 20:
+        k += 1
+print(k)
